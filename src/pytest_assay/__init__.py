9de9a7b3577bd46f76f00df02db62483c04a97db
lars20070 <l.nilse@dunelm.org.uk>
2026-03-29 07:32:34 +0200

    expose further components

--- src/pytest_assay/__init__.py
+++ src/pytest_assay/__init__.py
@@ -1,6 +1,17 @@
 """pytest-assay public interface"""
 
-from pytest_assay.evaluators.bradleyterry import EvalGame, EvalPlayer, EvalTournament
-from pytest_assay.models import Evaluator, EvaluatorInput, Readout
+from pytest_assay.evaluators.bradleyterry import BradleyTerryEvaluator, EvalGame, EvalPlayer, EvalTournament
+from pytest_assay.evaluators.pairwise import PairwiseEvaluator
+from pytest_assay.models import AssayContext, Evaluator, EvaluatorInput, Readout
 
-__all__ = ["EvalGame", "EvalPlayer", "EvalTournament", "Evaluator", "EvaluatorInput", "Readout"]
+__all__ = [
+    "AssayContext",
+    "BradleyTerryEvaluator",
+    "EvalGame",
+    "EvalPlayer",
+    "EvalTournament",
+    "Evaluator",
+    "EvaluatorInput",
+    "PairwiseEvaluator",
+    "Readout",
+]
